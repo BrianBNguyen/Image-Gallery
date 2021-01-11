e6cde4a98e01f161e88d62aa61c65e26d6b720a7
BrianBNguyen <59449776+BrianBNguyen@users.noreply.github.com>
2021-01-11 14:33:50 -0500

    Update image gallery table.sql

--- image gallery table.sql
+++ image gallery table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE image_gallery(
+    image_id VARCHAR(250) NOT NULL,
     username VARCHAR(60) NOT NULL,
     image_name VARCHAR(180) NOT NULL,
-    private_image BIT (1) NOT NULL
+    private_image BIT (1) NOT NULL,
+    PRIMARY KEY (image_id)
     
   );
