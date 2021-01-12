ff25f140aa9515e5cfcfd9618820942abe082930
BrianBNguyen <59449776+BrianBNguyen@users.noreply.github.com>
2021-01-12 12:57:12 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -35,35 +35,42 @@ include 'header.php';
             </td>
             <td>
                  <input type='text' name='username' id='username'>
+
             </td>
         </tr>
         <tr>
             <td>
             <label for="col">Password: 
+
             </td>
             <td>
             <input type='password' name='password' id='password' >
+
             </td>
         </tr>
         <tr>
             <td>
             <label for="col">Confrim New Password: 
+
             </td>
             <td>
             <input type='password' name='password2' id='password2'>
+
             </td>
         </tr>
     </table>
         <br>
             <br>
             <button type="submit"  id="create_account" name="create_account">Create Account</button>
+
     </form>
-    <br>
-    <br>
+
 </div>
 </body>
 
+<div style = "width:50%; margin: auto; ">
 <?php
+
 // user tried to create new acount
 if(isset($_POST['create_account'])){
     // grab user info
@@ -105,10 +112,10 @@ if(isset($_POST['create_account'])){
         <script>
         window.location.replace("https://shopifybackendchallenge.000webhostapp.com/login_page.php");
         </script>
-        ';   
-    }
+        ';    }
 }
 
 ?>
+</div>
 
 
